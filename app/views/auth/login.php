a36a621fe13dbff3a4096c9e6a1a665829ba34c2
Palguna1121 <palguna2212@gmail.com>
2024-06-19 16:30:03 +0800

    ui login & register

--- app/views/auth/login.php
+++ app/views/auth/login.php
@@ -1,14 +1,45 @@
-<?php Flasher::flash();?>
-<h1>halaman login nihh</h1>
-<form action="<?= BASEURL ?>/login/store" method="post">
-    <div class="mb-3">
-        <label for="email" class="form-label">Email</label>
-        <input type="email" class="form-control" id="email" name="email">
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="<?= BASEURL ?>/css/auth.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+    <title>Login</title>
+  </head>
+  <body>
+    <div class="wrapper">
+      <div class="container main">
+        <div class="row">
+          <div class="col-md-6 side-image"></div>
+
+          <div class="col-md-6 right">
+            <div class="input-box">
+              <form action="<?= BASEURL ?>/login/store" method="post">
+                <header>Login account</header>
+                <div class="mb-2">
+                    <?php Flasher::flash(); ?>    
+                </div>
+                <div class="input-field">
+                  <!-- <input type="text" class="input" id="email" required="" autocomplete="off" /> -->
+                  <input type="email" class="input" id="email" name="email" required />
+                  <label for="email">Email</label>
+                </div>
+                <div class="input-field">
+                  <input type="password" class="input" id="password" name="password" required />
+                  <label for="pass">Password</label>
+                </div>
+                <div class="input-field">
+                  <button type="submit" class="submit">Sign In</button>
+                </div>
+                <div class="register">
+                  <span>Don't have an account? <a href="<?= BASEURL ?>/register">Sign up now</a></span>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="mb-3">
-        <label for="password" class="form-label">Password</label>
-        <input type="password" class="form-control" id="password" name="password">
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-    <a href="<?= BASEURL ?>/register">belum ada akun yaaaa</a>
-</form>
+  </body>
+</html>
--- app/views/auth/register.php
+++ app/views/auth/register.php
@@ -1,22 +1,52 @@
-<?php Flasher::flash();?>
-<h1>halaman register nihh</h1>
-<form action="<?= BASEURL ?>/register/store" method="post">
-    <div class="mb-3">
-        <label for="nama" class="form-label">Nama</label>
-        <input type="text" class="form-control" id="nama" name="nama">
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="<?= BASEURL ?>/css/auth.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+    <title>Login</title>
+  </head>
+  <body>
+    <div class="wrapper">
+      <div class="container main">
+        <div class="row">
+          <div class="col-md-6 side-image"></div>
+
+          <div class="col-md-6 right">
+            <div class="input-box">
+               <form action="<?= BASEURL ?>/register/store" method="post">
+                <header>Login account</header>
+                <div class="mb-2">
+                    <?php Flasher::flash(); ?>    
+                </div>
+                <div class="input-field">
+                    <input type="text" class="input" id="nama" name="nama" required>
+                    <label for="Nama">Nama</label> 
+                </div> 
+                <div class="input-field">
+                    <input type="text" class="input" id="no_telp" name="no_telp" required>
+                    <label for="no_telp">Nomor Telpon</label> 
+                </div> 
+                <div class="input-field">
+                    <input type="text" class="input" id="email" name="email" required>
+                    <label for="email">Email</label> 
+                </div> 
+                <div class="input-field">
+                    <input type="password" class="input" id="password" name="password" required>
+                    <label for="password">Password</label>
+                </div> 
+                <div class="input-field">
+                  <button type="submit" class="submit">Sign Up</button>
+                </div>
+                <div class="register">
+                    <span>Already have an account? <a href="<?= BASEURL ?>/login">Sign in</a></span>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="mb-3">
-        <label for="password" class="form-label">Password</label>
-        <input type="password" class="form-control" id="password" name="password">
-    </div>
-    <div class="mb-3">
-        <label for="email" class="form-label">Email</label>
-        <input type="email" class="form-control" id="email" name="email">
-    </div>
-    <div class="mb-3">
-        <label for="no_telp" class="form-label">No Telp</label>
-        <input type="tel" class="form-control" id="no_telp" name="no_telp">
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-    <a href="<?= BASEURL ?>/login">sudah punya akun?</a>
-</form>
+  </body>
+</html>
