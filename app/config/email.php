7030baf409f7e1e878deece07b1e871a8905aa37
Palguna1121 <palguna2212@gmail.com>
2024-07-01 16:35:15 +0800

    email ku gais

---
+++ app/config/email.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'host' => 'smtp.gmail.com',
+    'username' => 'emailcatcher311@gmail.com', // akun email
+    'password' => 'mvblnngqjximwjgf', // app password dari verivikai 2 langkah email ini
+    'port' => 465,
+    'encryption' => 'ssl', // atau 'tls' tergantung penyedia layanan email
+    'from_email' => 'emailcatcher311@gmail.com',
+    'from_name' => 'Belitiket',
+];
