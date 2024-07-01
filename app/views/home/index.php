9acca2689442837b57f21c1858dd596d9461e1f0
SriAndayani <sriradhika048@gmail.com>
2024-07-01 17:01:36 +0800

    ubah dikit di alur

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -93,7 +93,7 @@
             <div class="container">
                 <div class="text-center">
                     <h2 class="section-heading text-uppercase">Alur Pemesanan Tiket</h2>
-                    <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                    <h3 class="section-subheading text-muted">login dulu</h3>
                 </div>
                 <ul class="timeline">
                     <li>
